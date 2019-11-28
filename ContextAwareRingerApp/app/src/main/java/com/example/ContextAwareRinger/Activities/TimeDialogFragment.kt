e7805b147ce8c0270f48da360c2d03c257327aa6
Elliot <elliotjhay@gmail.com>
2019-11-28 00:26:07 -0500

    Finished implementing TimePicker

--- ContextAwareRingerApp/app/src/main/java/com/example/ContextAwareRinger/Activities/TimeDialogFragment.kt
+++ ContextAwareRingerApp/app/src/main/java/com/example/ContextAwareRinger/Activities/TimeDialogFragment.kt
@@ -2,20 +2,22 @@ package com.example.ContextAwareRinger.Activities
 
 import android.app.Dialog
 import android.app.TimePickerDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import android.widget.TimePicker
+import android.widget.*
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.ContextAwareRinger.Data.LocationData
+import com.example.ContextAwareRinger.Data.TimeData
 import com.example.ContextAwareRinger.R
 import com.example.ContextAwareRinger.TimeViewModel
 import java.util.*
+import android.media.AudioManager
 
 
 class TimeDialogFragment: DialogFragment () {
@@ -25,12 +27,16 @@ class TimeDialogFragment: DialogFragment () {
     private var hour : Int? = -1
     private var minute : Int? = -1
 
+    private lateinit var timeButton : Button
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val rootView = inflater!!.inflate(R.layout.time_dialog, container, false)
-        val timeButton : Button = rootView.findViewById(R.id.startTimeButton)
-        val submitButton : Button = rootView.findViewById(R.id.buttonSubmitTime)
 
+        val submitButton : Button = rootView.findViewById(R.id.buttonSubmitTime)
+        val radioGroup : RadioGroup = rootView.findViewById(R.id.volumeRadioGroupTime)
+        val spinner : Spinner = rootView.findViewById(R.id.timeSelectionSpinner)
+        timeButton = rootView.findViewById(R.id.startTimeButton)
         //Initialize viewmodel and observe the time data
         viewModel = ViewModelProviders.of(activity!!).get(TimeViewModel::class.java)
 
@@ -44,8 +50,12 @@ class TimeDialogFragment: DialogFragment () {
         //Update minute and the textview when minute is changed by the timePicker Fragment
         val minuteObserver = Observer { min: Int? ->
             Log.i(TAG, "Hour updated")
+            //Store the value in this fragment to be used later
             minute = min
+            //Get a reference to the time select button to update the text
             val timeText : TextView = rootView.findViewById(R.id.startTimeButton)
+
+            //Check that appropriate values have been provided
             if (hour != null && minute != null && hour != -1 && minute != 1) {
                 var hourStr = ""
 
@@ -65,12 +75,13 @@ class TimeDialogFragment: DialogFragment () {
                     minuteStr = "0" + minute.toString()
                 }
 
-
+                //Update the button text to indicate the user's selection
                 timeText.text = "Time Selected - " + hourStr + ":" + minuteStr
             }
 
         }
 
+        //Attach observers to the viewmodel
         viewModel.hour.observe(this, hourObserver)
         viewModel.minute.observe(this, minuteObserver)
 
@@ -93,17 +104,78 @@ class TimeDialogFragment: DialogFragment () {
 
         //Set onclick for the submit button
         submitButton.setOnClickListener {
-//            val
+            //Get selected volume
+            var selected : RadioButton? = null
+
+            if (radioGroup.checkedRadioButtonId != -1) {
+                selected = rootView.findViewById(radioGroup.checkedRadioButtonId)
+            }
+
+            var ringerMode : Int? = null
+            //Convert radio button option to an int
+            if (selected != null) {
+                if (selected.text == "Do Not Disturb") {
+                    ringerMode = AudioManager.RINGER_MODE_SILENT
+                } else if (selected.text == "Vibrate") {
+                    ringerMode = AudioManager.RINGER_MODE_VIBRATE
+                } else {
+                    ringerMode = AudioManager.RINGER_MODE_NORMAL
+                }
+            }
+
+            //Get repetition from spinner
+            var repetitionInterval : Int = getRepetitionInterval(spinner)
+
+            val workKey = UUID.randomUUID().toString()
+
+            //Ensure that all user input is provided
+            if (radioGroup.checkedRadioButtonId != -1 && hour != -1 && minute != -1) {
+                //Close the dialog box
+                dismiss()
+
+                Log.i(TAG, "Time object created with Hour: " + hour + ", Minute: " + minute + ", Volume: " + selected!!.text + ", Interval: " + spinner.selectedItem.toString())
+
+                //TODO: Create list adapter stuff
+                val time = TimeData(hour!!, minute!!, repetitionInterval, workKey, ringerMode!!)
+            } else {
+                Toast.makeText(
+                    context!!,
+                    "Please enter all requested information",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
+
         return rootView
     }
 
+    //Converts the selected spinner option to an int
+    private fun getRepetitionInterval(spinner : Spinner) : Int {
+        val text = spinner.selectedItem.toString()
+        when (text) {
+            "Daily" -> return com.example.ContextAwareRinger.DAILY
+            "Weekends" -> return com.example.ContextAwareRinger.WEEKEND
+            "Week Days" -> return com.example.ContextAwareRinger.WEEKDAY
+            "Monday" -> return Calendar.MONDAY
+            "Tuesday" -> return Calendar.TUESDAY
+            "Wednesday" -> return Calendar.WEDNESDAY
+            "Thursday" -> return Calendar.THURSDAY
+            "Friday" -> return Calendar.FRIDAY
+            "Saturday" -> return Calendar.SATURDAY
+            "Sunday" -> return Calendar.SUNDAY
+        }
+
+        //Should never reach this point
+        throw Exception("No valid value for repetition interval found")
+
+    }
 }
 
-// Time Picking Fragment
+
 class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
     val TAG = "Time Picker Fragment"
     private lateinit var viewModel: TimeViewModel
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         viewModel = ViewModelProviders.of(activity!!).get(TimeViewModel::class.java)
         val c = Calendar.getInstance()
@@ -114,9 +186,10 @@ class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener 
         return TimePickerDialog(activity, this, hourOfDay, minute,false)
     }
 
-    // Called by TimePickerDialog when user sets the time
+
     override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
         Log.i(TAG, "Updating times values in view model")
+        //Update the values of hour and minute in the viewModel so that timedialogfragment gets updated
         viewModel.hour.value = hourOfDay
         viewModel.minute.value = minute
     }
