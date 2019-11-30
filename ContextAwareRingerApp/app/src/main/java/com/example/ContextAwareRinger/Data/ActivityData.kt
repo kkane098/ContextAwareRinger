1b601b52567fbb76b6ba6db86fbf6e14944de7a9
PRIT PATEL <pritpatel9897@gmail.com>
2019-11-30 13:27:24 -0500

    Activity UI

--- ContextAwareRingerApp/app/src/main/java/com/example/ContextAwareRinger/Data/ActivityData.kt
+++ ContextAwareRingerApp/app/src/main/java/com/example/ContextAwareRinger/Data/ActivityData.kt
@@ -2,5 +2,9 @@ package com.example.ContextAwareRinger.Data
 
 import kotlinx.serialization.*
 
+val ACTIVITY_VEHICLE = 0
+val ACTIVITY_FOOT = 1
+val ACTIVITY_BICYCLE = 2
+
 @Serializable
 data class ActivityData(val activityType: Int, val fenceKey: String, val ringerMode: Int)
